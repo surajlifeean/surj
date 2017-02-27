153c1afe9bce0eb5f44c5e5e6fff14dccfb8d044
surajlifeean <surajjeswara@yahoo.com>
2017-02-27 08:40:33 -0800

    create post page

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+@section('content')
+
+<div class="row">
+ <div class="col-md-8 col-md-offset-2">
+   <h1> create new post</h1>
+    {!! Form::open(array('route' => 'posts.create')) !!}
+    {{ Form::label('title','Title:')}}
+    {{ Form::text('title',null,array('class'=>'form-control'))}}
+
+    {{ Form::label('body',"Post Body:")}}
+    {{ Form::textarea('body',null,array('class'=>'form-control'))}}
+
+
+    {{ Form::submit('create post',array('class'=>'btn btn-success btn-lg btn-block','style'=>'margin-top:20px;'))}}
+
+    
+    
+{!! Form::close() !!}
+@endsection
---
+++ resources/views/posts/login.blade.php
@@ -0,0 +1,78 @@
+@extends('main')
+@section('content')
+    <div class="row">
+       <div class="col-md-12">
+         <div class="jumbotron">
+           <h1>TIER5</h1>
+             <p>...</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+         </div>
+       </div>
+    </div>
+    <div class="row">
+       <div class="col-md-8">
+         <div class="post">
+           <h1>Services</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+       </div>
+        <hr>
+       <div class="col-md-8">
+         <div class="post">
+           <h1>About</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+       </div>
+ 
+         <hr>
+    
+       <div class="col-md-8">
+         <div class="post">
+           <h1>Process</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+       </div>
+      
+       <div class="col-md-8">
+         <div class="post">
+           <h1>Products</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+       </div>
+       <div class="col-md-8">
+         <div class="post">
+           <h1>Work</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+        </div>
+         <div class="col-md-8">
+         <div class="post">
+           <h1 id="Testimonials">Testimonials</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+        </div>
+         <div class="col-md-8">
+         <div class="post">
+           <h1>Contact</h1>
+             <p>he have lots of services to offer</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+        </div> 
+
+     <!-- <div class="col-md-3 col-md-offset-1">
+         <div class="post">
+           <h1>Notification</h1>
+             <p>.............</p>
+              <p><a class="btn btn-primary" href="#" role="button">more</a></p>
+         </div>
+       </div> -->
+             
+ 
+    </div>
+    @endsection
